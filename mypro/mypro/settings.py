229fe8ef466ace5c2984a2d1807ed1b24e33dcb8
Seleem Aleshinloye <seleemmiah1@gmail.com>
2023-06-19 20:45:11 +0100

    A construction website in django

--- mypro/mypro/settings.py
+++ mypro/mypro/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'mypro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mypro',
+        'USER': 'postgres',
+        'PASSWORD': 'evaporation',
+        'HOST': 'localhost'
     }
 }
 
